8d9aa94b0b3e97e16924e1b102b7a2b977ebbfdf
Ashley Smith <ashlynn4567@gmail.com>
2022-03-15 14:31:16 -0500

    finished adding date audit functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,8 +15,31 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
+};
+
+
+// check task due date to color task appropriately
+var auditTask = function(taskEl) {
+  // get date from task element
+  var date = $(taskEl).find("span").text().trim();
+  
+  // convert to moment object at 5:00pm 
+  var time = moment(date, "L").set("hour", 17);
+
+  // apply new class if task is near/over due date
+  // if the task due date has passed, make red
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  
+  // if the task due date is coming soon, make yellow
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  };
 };
 
 
@@ -270,6 +293,9 @@ $(".list-group").on("change", "input[type='text']", function () {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // pass task's <li> element into auditTask() to check new due date after edited
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 
